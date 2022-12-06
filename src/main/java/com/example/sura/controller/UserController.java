31041b30e104f520af5fe3c89615b78d26a0d18d
SebasTamayoA <sebastamayo37@gmail.com>
2022-12-05 23:25:59 -0500

    Configuración inicial

--- src/main/java/com/example/sura/controller/UserController.java
+++ src/main/java/com/example/sura/controller/UserController.java
@@ -1,2 +1,24 @@
-package com.example.sura.controller;public class UserController {
+package com.example.sura.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value= "api/user")
+
+public class UserController {
+    @GetMapping(value = "/test")
+    public ResponseEntity<String> testGet() {
+        return ResponseEntity.status(HttpStatus.OK).body("Test Get");
+    }
+
+    @PostMapping(value = "/test")
+    public ResponseEntity<String> testPost() {
+        return ResponseEntity.status(HttpStatus.OK).body("Test Post");
+    }
+
 }
